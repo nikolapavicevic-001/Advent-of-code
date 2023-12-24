c7b47a499d1f0b87f8b63add49a338d6b050a82c
Nikola Pavicevic <nikolapavicevic95@gmail.com>
2023-12-24 21:35:00 +0100

    2023 Day1 part 2

--- Day1/src/main.rs
+++ Day1/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 fn read_from_file() -> io::Result<Vec<String>> {
@@ -13,9 +14,10 @@ fn read_from_file() -> io::Result<Vec<String>> {
     Ok(content)
 
 }
+
 #[allow(non_snake_case)]
-fn main() {
-    let content = read_from_file().expect("No file found");
+#[allow(dead_code)]
+fn calculateFirstPart(content: &Vec<String>) -> u32 {
     let mut sum = 0;
     for line in content{
         let mut numbers : Vec<u32> = Vec::new();
@@ -26,5 +28,52 @@ fn main() {
         }
         sum += numbers[0] * 10 + numbers[numbers.len() - 1];
     }
+    sum
+}
+
+#[allow(non_snake_case)]
+fn calculateSecondPart(content: &Vec<String>) -> u32 {
+    let mut sum = 0;
+    let possibilites = HashMap::from([
+        ("1",1),
+        ("2",2),
+        ("3",3),
+        ("4",4),
+        ("5",5),
+        ("6",6),
+        ("7",7),
+        ("8",8),
+        ("9",9),
+        ("one",1),
+        ("two",2),
+        ("three",3),
+        ("four",4),
+        ("five",5),
+        ("six",6),
+        ("seven",7),
+        ("eight",8),
+        ("nine",9)
+    ]);
+    for line in content{
+        let mut numbers : Vec<u32> = Vec::new();
+        println!("{line}");
+        for i in 0..line.len(){
+            for(key,value) in possibilites.iter(){
+                if line[i..].starts_with(key) {
+                    println!("{}",*value);
+                    numbers.push(Some(*value).unwrap())
+                }
+            }
+        }
+        println!("SUM {} {} ",numbers[0], numbers[numbers.len() - 1]);
+        sum += numbers[0] * 10 + numbers[numbers.len() - 1];
+    }
+    sum
+}
+#[allow(non_snake_case)]
+fn main() {
+    let content = read_from_file().expect("No file found");
+    // let sum = calculateFirstPart(&content);
+    let sum = calculateSecondPart(&content);
     println!("Result is {sum}")
 }
